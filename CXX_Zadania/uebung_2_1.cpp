6bb79946281a61b3eb52400698b7fecbfd98a08e
rucinski_p <rutek914@hotmail.com>
2023-03-27 18:10:40 +0200

    pcrobimy

--- CXX_Zadania/uebung_2_1.cpp
+++ CXX_Zadania/uebung_2_1.cpp
@@ -4,11 +4,22 @@ using namespace std;
 
 
 
-
+void zerujLiczby(int* wskLiczba1, int* wskLiczba2){
+    *wskLiczba1 = 0;
+    *wskLiczba2 = 0;
+    cout << "wewnatrz funkcji " << *wskLiczba1 << " " << *wskLiczba2 << endl;
+}
 
 int main(int argc, char* argv[]){
+    int liczba1 = 10;
+    int liczba2 = 20;
+    int* wskLiczba1 = &liczba1;
+    int* wskLiczba2 = &liczba2;
+    cout << "wartosc liczby ze wskaznika " << *wskLiczba1 << " "<< *wskLiczba2<<endl;
 
-
+    zerujLiczby(wskLiczba1, wskLiczba2);
+    cout << "Liczby po funkcji "<<liczba1<<" "<<liczba2<<endl;
+    return 0;
 
 
 
