2fed6876a5574ef1ddf722c6738f935991a9976d
Piotr <rutek914@hotmail.com>
2023-03-30 15:24:31 +0200

    referencjaPierwsza

--- CXX_Testy/book_1.cpp
+++ CXX_Testy/book_1.cpp
@@ -2,9 +2,21 @@
 
 using namespace std;
 
+const double pi = 3.1416;
 
+void Area(double Radius, double& wynik);
 
 int main(){
     
+double Radius = 5;
 
+double poleKola = 0;
+Area(Radius, poleKola);
+cout<< poleKola<<endl;
+
+
+}
+
+void Area(double Radius, double& wynik){
+    wynik = pi*Radius*Radius;
 }
