2b03066486ae430fbba1005a386c5968bcfe226d
Piotr <rutek914@hotmail.com>
2023-04-06 14:35:26 +0200

    h

--- CXX_Testy/book_1.cpp
+++ CXX_Testy/book_1.cpp
@@ -4,19 +4,25 @@ using namespace std;
 
 const double pi = 3.1416;
 
-void Area(double Radius, double& wynik);
+void Area(double promien, double& wynik);
+void Area(double promien, double wysokosc, double& wynik);
 
 int main(){
     
-double Radius = 5;
-
-double poleKola = 0;
-Area(Radius, poleKola);
-cout<< poleKola<<endl;
+double wyn = 0;
+const double radius = 1;
+const double high = 5;
+Area(radius, wyn);
+cout << wyn << " kula"<< endl;
+Area(radius, high, wyn);
+cout << wyn << " walec" << endl;
 
 
 }
 
-void Area(double Radius, double& wynik){
-    wynik = pi*Radius*Radius;
+void Area(const double promien, double& wynik){
+    wynik = (4*pi*promien*promien*promien)/3;
 }
+void Area(const double promien,const double wysokosc, double& wynik){
+    wynik = pi*promien*promien*wysokosc;
+} 
