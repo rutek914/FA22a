69426503fbf9f5fb06da8dc96d085ab7a903df64
Piotr <rutek914@hotmail.com>
2023-06-07 07:11:49 +0200

    Cmake_poczatek

---
+++ CXX_Testy/Praca/Workspace/Cmake/build/CMakeFiles/Executable.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/rutek/OneDrive/Dokumenty/vscode_git/FA22a/CXX_Testy/Praca/Workspace/Cmake/main.cpp" "CMakeFiles/Executable.dir/main.cpp.o" "gcc" "CMakeFiles/Executable.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CXX_Testy/Praca/Workspace/Cmake/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Helllloooo Cmake!" << std::endl;
+
+    return 0;
+}
